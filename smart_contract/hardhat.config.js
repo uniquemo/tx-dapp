c6619cf9a0524f29f529219f329226d7039b5d57
unique.mo <myx3901051@qq.com>
2023-01-10 21:59:06 +0800

    feat(smart_contract): add contract logic

---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,16 @@
+/**
+ * npx hardhat run scripts/deploy.js --network goerli
+ * 部署地址：0x1474750D45D1e89fBa710Fab26FcF2b4F72bEfe8
+ */
+
+require('@nomiclabs/hardhat-waffle');
+
+module.exports = {
+  solidity: '0.8.17',
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/nNnvQWSOhSxGEzK4ubv18AT7fil55PBv',
+      accounts: ['5ecac5c2be0e1df84fb6f1639fddd87e4640cace3e9111724b53ea4a09d35ac3'],
+    },
+  },
+};
