5fb7b603161988d7414536a0b250673f3b761f3b
unique.mo <myx3901051@qq.com>
2023-01-11 15:15:30 +0800

    doc(smart_contract): add some comment

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -9,7 +9,9 @@ module.exports = {
   solidity: '0.8.17',
   networks: {
     goerli: {
+      // url是在alchemy平台创建应用后的网络http地址
       url: 'https://eth-goerli.g.alchemy.com/v2/nNnvQWSOhSxGEzK4ubv18AT7fil55PBv',
+      // accounts为MetaMask中，account details => Export private key的值
       accounts: ['5ecac5c2be0e1df84fb6f1639fddd87e4640cace3e9111724b53ea4a09d35ac3'],
     },
   },
