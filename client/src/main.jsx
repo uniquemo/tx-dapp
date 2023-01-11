b3d6a9c438592e09c726ea58e9e46c79c3daec78
unique.mo <myx3901051@qq.com>
2023-01-11 14:58:53 +0800

    feat(client): add transaction logic

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+
+import { TransactionsProvider } from './context/TransactionContext';
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <TransactionsProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </TransactionsProvider>,
 )
---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractAddress = '0x1474750D45D1e89fBa710Fab26FcF2b4F72bEfe8';
+export const contractABI = abi.abi;
